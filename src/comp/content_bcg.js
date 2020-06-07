f1ef170a33c27c0e8da3af869ac4fa3be2cad111
FridaCai <525311175@qq.com>
2020-06-07 13:36:56 +0800

    update

--- src/comp/content_bcg.js
+++ src/comp/content_bcg.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './content_bcg.css';
 import FormularCanvas from './formular_canvas';
+import img from '../assets/bcg_img.png';
 
 function ContentBCG() {
     return (
       <div className="contentBCG">
         <div className="constainer left">
-            <span className='mark'>输入</span>
-            <div className='line'><span>主管直径D(mm)</span><input></input></div>
+            <span className="badge badge-primary badge-lg">输入</span>
+            <div className='line' style={{marginTop: '20px'}}><span>主管直径D(mm)</span><input></input></div>
             <div className='line'><span>调音管直径Db(mm)</span><input></input></div>
             <div className='line'><span>调音管长度L(mm)</span><input></input></div>
-            <button >开始计算</button>
-            <img src='../../assets/bcg.png'/>
+            <div className='line'><button type="button" className="btn btn-primary btn-sm">开始计算</button></div>
+            <img src={img}/>
         </div>
         <div className="constainer right">
-            <span className='mark'>输出</span>
-            <div className='line'><span>调音频率f(HZ)</span><input></input></div>
+            <span className="badge badge-primary badge-sm">输出</span>
+            <div className='line' style={{marginTop: '20px'}}><span>调音频率f(HZ)</span><input></input></div>
             <div className='line'><span>一级谐波(HZ)</span><input></input></div>
-            <span>二级谐波(HZ)</span><input></input>
+            <div className='line'><span>二级谐波(HZ)</span><input></input></div>
             <FormularCanvas/>
         </div>
       </div>
---
+++ src/comp/formular_canvas_deprecated.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import './formular_canvas.css';
+
+var Plot = {  
+    //画布，所有被画出来的元素都append到这个container  
+  container: null,  
+  //原点x  
+  ox: 0,  
+  //原点y  
+  oy: 300,  
+  //坐标颜色  
+  baseLineColor: 'black',  
+  //画笔颜色  
+  brushColor: 'red',  
+  //画笔粗细  
+  brushWeight: 1,  
+  //baseLineX，baseLineY保存坐标线，用于坐标移位  
+  baseLineX: null,  
+  baseLineY: null
+};  
+
+function FormularCanvas_sin(props){
+    let points = []; //calc points based on props, minor baseline offset.
+    let angle = 360;
+
+    for(let x=1; x<angle; x++){
+        let y = Math.sin(x/180*Math.PI)*100;
+        points.push({x: x+Plot.ox, y: y+Plot.oy+ 300})
+    }
+
+    return (<div className='canvas'>
+        <div className='baseline x' style={{top: Plot.oy}}></div>
+        <div className='baseline y' style={{left: Plot.ox}}></div>
+        {
+            points.map(point => (
+                <div key={point.x} className='point' style={{left: point.x - Plot.ox, top: point.y - Plot.oy}}></div>
+            ))
+        }
+    </div>)
+}
+
+function FormularCanvas(props){
+    // const {L, D, Db} = props;
+    let L = 200;
+    let D = 70;
+    let Db = 40;
+    let C = 343;
+
+
+    let points = []; //calc points based on props, minor baseline offset.
+    for(let f=1; f<500; f++){
+        let tanP = Math.PI * L * f / 500 / C;
+        let logP = 1 + 0.25 * Db * Db / D / D * Math.tan(tanP) * Math.tan(tanP);
+        let TL = 10 * Math.log10(logP)
+        points.push({x: f + Plot.ox, y: TL + Plot.oy})
+    }
+
+    return (<div className='canvas'>
+        <div className='baseline x' style={{top: Plot.oy}}></div>
+        <div className='baseline y' style={{left: Plot.ox}}></div>
+        {
+            points.map(point => (
+                <div key={point.x} className='point' style={{left: point.x - Plot.ox, top: point.y - Plot.oy}}></div>
+            ))
+        }
+    </div>)
+
+
+
+
+
+
+
+}
+  
+export default FormularCanvas;
